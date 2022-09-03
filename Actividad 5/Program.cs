1840261a8d377d19697763f834eb56b1a6d20eb3
aayalac <aayalac@misena.edu.co>
2022-09-03 12:39:58 -0500

    Agregar archivos de proyecto.

---
+++ Actividad 5/Program.cs
@@ -0,0 +1,10 @@
+﻿using Actividad_5.Models;
+using Actividad_5.Controllers;
+using Actividad_5.Views.Vehicle;
+
+Console.WriteLine("Programa de registro de buses para el SITP");
+Console.WriteLine("----------------------------------------------");
+
+
+BusController controller = new BusController(new CrearBus());
+controller.Crear("1", "SE14", Estacion.Build("1","carrera30","SE14"));
